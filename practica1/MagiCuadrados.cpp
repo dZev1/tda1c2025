d7fb03ccdff098374732b5a57959dda8ae62488a
dZev1 <diegozevallos04@gmail.com>
2025-04-17 14:49:56 -0300

    asdadsadsads

--- practica1/MagiCuadrados.cpp
+++ practica1/MagiCuadrados.cpp
@@ -1,63 +1,99 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int RES;
 const int n = 3;
 int cuadrado[n][n] = {};
-int numDisponibles[n * n] = {};
+vector<int> numDisponibles;
 
-
-bool esMagico(const int cuadrado[n][n]) {
-    int sumaMagica = n * (n * n + 1) / 2;
-
-    // Verificar filas
-    for (int i = 0; i < n; ++i) {
-        int sumaFila = 0;
-        for (int j = 0; j < n; ++j) {
-            sumaFila += cuadrado[i][j];
-        }
-        if (sumaFila != sumaMagica) return false;
-    }
-
-    // Verificar columnas
-    for (int j = 0; j < n; ++j) {
-        int sumaColumna = 0;
-        for (int i = 0; i < n; ++i) {
-            sumaColumna += cuadrado[i][j];
-        }
-        if (sumaColumna != sumaMagica) return false;
-    }
-
-    // Verificar diagonales
+bool esMagico(int cuadrado[n][n])
+{
     int sumaDiagonal1 = 0, sumaDiagonal2 = 0;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         sumaDiagonal1 += cuadrado[i][i];
-        sumaDiagonal2 += cuadrado[i][n - i - 1];
+        sumaDiagonal2 += cuadrado[i][n - 1 - i];
     }
-    if (sumaDiagonal1 != sumaMagica || sumaDiagonal2 != sumaMagica) return false;
+    if (sumaDiagonal1 != sumaDiagonal2)
+        return false;
 
+    for (int i = 0; i < n; ++i)
+    {
+        int sumaFila = 0, sumaColumna = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            sumaFila += cuadrado[i][j];
+            sumaColumna += cuadrado[j][i];
+        }
+        if (sumaFila != sumaColumna || sumaFila != sumaDiagonal1)
+            return false;
+    }
     return true;
 }
 
-void MagiCuadrados(int cuadrado[n][n], int fila, int col, int usados) {
-    if (fila == n) {
-        if (esMagico(cuadrado)) {
-            ++RES;
+bool filaValida(int cuadrado[n][n], int i)
+{
+    vector<bool> numUsados(n + 1, false);
+    for (int j = 0; j < n; ++j)
+    {
+        if (cuadrado[i][j] != 0)
+        {
+            if (numUsados[cuadrado[i][j]])
+                return false;
+            numUsados[cuadrado[i][j]] = true;
+        }
+    }
+    return true;
+}
+
+bool columnaValida(int cuadrado[n][n], int j)
+{
+    vector<bool> numUsados(n + 1, false);
+    for (int i = 0; i < n; ++i)
+    {
+        if (cuadrado[i][j] != 0)
+        {
+            if (numUsados[cuadrado[i][j]])
+                return false;
+            numUsados[cuadrado[i][j]] = true;
+        }
+    }
+    return true;
+}
+
+void MagiCuadrados(int cuadrado[n][n], int i, int j, vector<int> numDisponibles)
+{
+    if (i == n)
+    {
+        if (esMagico(cuadrado))
+        {
+            RES++;
         }
         return;
     }
 
-    int siguienteFila = (col == n - 1) ? fila + 1 : fila;
-    int siguienteCol = (col == n - 1) ? 0 : col + 1;
+    for (int k : numDisponibles)
+    {
+        cuadrado[i][j] = k;
+        vector<int> numDisponiblesAux = numDisponibles;
+        // Eliminar el número k de la lista de números disponibles
+        numDisponiblesAux.erase(remove(numDisponiblesAux.begin(), numDisponiblesAux.end(), k), numDisponiblesAux.end());
 
-    for (int i = 0; i < n * n; ++i) {
-        if (!(usados & (1 << i))) {
-            cuadrado[fila][col] = numDisponibles[i];
-            MagiCuadrados(cuadrado, siguienteFila, siguienteCol, usados | (1 << i));
-            cuadrado[fila][col] = 0;
+        if (filaValida(cuadrado, i) && columnaValida(cuadrado, j))
+        {
+            if (j == n - 1)
+            {
+                MagiCuadrados(cuadrado, i + 1, 0, numDisponiblesAux);
+            }
+            else
+            {
+                MagiCuadrados(cuadrado, i, j + 1, numDisponiblesAux);
+            }
         }
+        cuadrado[i][j] = 0;
     }
 }
 
@@ -65,12 +101,13 @@ int main()
 {
     for (int i = 1; i < (n * n) + 1; ++i)
     {
-        numDisponibles[i - 1] = i;
+        numDisponibles.push_back(i);
     }
     for (int elem : numDisponibles)
     {
         cout << elem << " ";
     }
-    MagiCuadrados(cuadrado, 0, 0, -1);
+    MagiCuadrados(cuadrado, 0, 0, numDisponibles);
+    cout << "Cantidad de cuadrados magicos: " << RES << endl;
     return 0;
 }
